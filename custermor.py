fb9bbb66a34d3e7b667e31c687a2e73d316882ab
xwp <xwp_fullstack@163.com>
2020-04-02 17:38:45 +0800

    构造函数

---
+++ custermor.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+class Check():
+    #成员变量
+    num1 = 0
+    s1 = ""
+ 
+    #成员方法
+    def show(self):
+        print("showing")
+ 
+    #构造函数
+    def __init__(self):
+        print("构造函数被执行了")
+        self.show()
+ 
+c = Check()          #构造函数被执行了
+c.show()   
